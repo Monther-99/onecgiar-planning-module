21564be3b129698e228b6acd9d76297055a615d3
tarequi <t.anshasi99@gmail.com>
2023-10-30 12:17:57 +0300

    https://codeobia.atlassian.net/browse/PLA-81

--- back-end/src/ipsr/ipsr.module.ts
+++ back-end/src/ipsr/ipsr.module.ts
@@ -3,9 +3,10 @@ import { IpsrController } from './ipsr.controller';
 import { IpsrService } from './ipsr.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ipsr } from 'src/entities/ipsr.entity';
+import { IpsrValue } from 'src/entities/ipsr-value.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ipsr])],
+  imports: [TypeOrmModule.forFeature([Ipsr, IpsrValue])],
   controllers: [IpsrController],
   providers: [IpsrService],
   exports: [IpsrService],
