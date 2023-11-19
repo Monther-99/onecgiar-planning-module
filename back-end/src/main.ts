0de6569cee970dbf0e678929c3d4d96c52b67df8
moayadnajd <moayad.najd@gmail.com>
2023-11-19 19:31:02 +0300

    enableCors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 import { env } from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, PATCH, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   await app.listen(env.APP_PORT);
 }
 bootstrap();
