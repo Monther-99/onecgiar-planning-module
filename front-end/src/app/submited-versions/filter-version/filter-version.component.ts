53fdd1ccfa067e13073309b31650bdc0e1e77335
mohammad saleem <mohammadsaleem712.cm@gmail.com>
2023-11-20 14:28:35 +0300

    https://codeobia.atlassian.net/browse/PLA-199

--- front-end/src/app/submited-versions/filter-version/filter-version.component.ts
+++ front-end/src/app/submited-versions/filter-version/filter-version.component.ts
@@ -1,73 +1,81 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { SubmissionService } from 'src/app/services/submission.service';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { SubmissionService } from "src/app/services/submission.service";
 @Component({
-  selector: 'app-filter-version',
-  templateUrl: './filter-version.component.html',
-  styleUrls: ['./filter-version.component.scss']
+  selector: "app-filter-version",
+  templateUrl: "./filter-version.component.html",
+  styleUrls: ["./filter-version.component.scss"],
 })
-export class FilterVersionComponent implements OnInit{
+export class FilterVersionComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private submissionService: SubmissionService,
-    private activatedRoute: ActivatedRoute,
+    private activatedRoute: ActivatedRoute
   ) {}
   filterForm: FormGroup = new FormGroup({});
 
   @Output() filters: EventEmitter<any> = new EventEmitter<any>();
 
-
   sort = [
-    { name: 'ID (lowest first)', value: 'id,ASC' },
-    { name: 'ID (highest first)', value: 'id,DESC' },
-    { name: 'Created at (Oldest)', value: 'created_at,ASC' },
-    { name: 'Created at (Newest)', value: 'created_at,DESC' },
+    { name: "ID (lowest first)", value: "id,ASC" },
+    { name: "ID (highest first)", value: "id,DESC" },
+    { name: "Created on (Oldest)", value: "created_at,ASC" },
+    { name: "Created on (Newest)", value: "created_at,DESC" },
   ];
 
   status = [
-    { name: 'Approved', value: 'Approved' },
-    { name: 'Rejected', value: 'Rejected' },
-    { name: 'Pending', value: 'Pending' },
+    { name: "Approved", value: "Approved" },
+    { name: "Rejected", value: "Rejected" },
+    { name: "Pending", value: "Pending" },
   ];
   submissions: any[] = [];
-  phases:any[] = [];
-  users:any[] = [];
+  phases: any[] = [];
+  users: any[] = [];
   params: any;
-  reportingYear:any;
+  reportingYear: any;
 
   async ngOnInit() {
     this.setForm();
     this.params = this.activatedRoute?.snapshot.params;
 
-    this.submissions = await this.submissionService.getSubmissionsByInitiativeId(this.params.id, null, null, null, false);
+    this.submissions =
+      await this.submissionService.getSubmissionsByInitiativeId(
+        this.params.id,
+        null,
+        null,
+        null,
+        false
+      );
 
-    this.submissions.map(d => {
+    this.submissions.map((d) => {
       this.phases.push(d.phase);
     });
-    this.phases = this.phases.filter((value, index, self) =>
-      index === self.findIndex((t) => (
-      t.id === value.id && t.name === value.name
+    this.phases = this.phases.filter(
+      (value, index, self) =>
+        index ===
+        self.findIndex((t) => t.id === value.id && t.name === value.name)
+    );
+
+    this.reportingYear = this.phases
+      .filter(
+        (value, index, self) =>
+          index ===
+          self.findIndex((t) => t.reportingYear === value.reportingYear)
       )
-    ));
-    
-    this.reportingYear = this.phases.filter((value, index, self) =>
-      index === self.findIndex((t) => (
-      t.reportingYear === value.reportingYear 
-    ))).map((d:any) => {return d.reportingYear});
-    
-    this.submissions.map(d => {
+      .map((d: any) => {
+        return d.reportingYear;
+      });
+
+    this.submissions.map((d) => {
       this.users.push(d.user);
     });
-    this.users = this.users.filter((value, index, self) =>
-      index === self.findIndex((t) => (
-      t.id === value.id && t.email === value.email
-      )
-    ));
-    
+    this.users = this.users.filter(
+      (value, index, self) =>
+        index ===
+        self.findIndex((t) => t.id === value.id && t.email === value.email)
+    );
   }
-
-
 
   setForm() {
     let time: any = null;
@@ -76,7 +84,7 @@ export class FilterVersionComponent implements OnInit{
       phase: [null],
       createdBy: [null],
       status: [null],
-      reportingYear: [null]
+      reportingYear: [null],
     });
     this.filterForm.valueChanges.subscribe(() => {
       console.log(this.filterForm.value);
@@ -91,6 +99,4 @@ export class FilterVersionComponent implements OnInit{
     this.filterForm.reset();
     this.filterForm.markAsUntouched();
   }
-
-
 }
