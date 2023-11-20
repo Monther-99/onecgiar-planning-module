7114554f42ed07d5e247eef97a13239ddc868742
moayadnajd <moayad.najd@gmail.com>
2023-11-20 17:55:00 +0300

    staging server

---
+++ front-end/src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    socket_path:"/socket.io",
+    socket_url:"https://planningtest-serv.ciat.cgiar.org",
+    api_url:'https://planningtest-serv.ciat.cgiar.org',
+    aws_cognito_link:'https://ost-toc.auth.us-east-1.amazoncognito.com',
+    aws_cognito_client_id:'7j40ekbpnbq4aegcsgjmm66c90',
+    aws_cognito_client_redirect_uri:'https://plannigtest.ciat.cgiar.org/auth'
+};
