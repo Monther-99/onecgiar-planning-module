56d2a5000fa4c54149b5f15e6063da56746dca8b
moayadnajd <moayad.najd@gmail.com>
2023-12-04 13:15:10 +0300

    Prod

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    socket_path:"/api/socket.io",
-    socket_url:"/",
-    api_url:'/api',
+    socket_path:"/socket.io",
+    socket_url:"https://api.planning.cgiar.org",
+    api_url:'https://api.planning.cgiar.org',
     aws_cognito_link:'https://ost-toc.auth.us-east-1.amazoncognito.com',
     aws_cognito_client_id:'7j40ekbpnbq4aegcsgjmm66c90',
-    aws_cognito_client_redirect_uri:'https://localhost:4200/auth'
+    aws_cognito_client_redirect_uri:'https://planning.cgiar.org/auth'
 };
