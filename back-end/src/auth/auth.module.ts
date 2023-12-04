8850d98c399ceb7cc25fb42ff272983c630530e7
moayadnajd <moayad.najd@gmail.com>
2023-12-04 14:40:36 +0300

    JWT_SECRET_KEY

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -3,23 +3,30 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { AwsStrategy } from './aws.strategy';
 import { HttpModule } from '@nestjs/axios';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     HttpModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '30d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET_KEY'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRATION_TIME'),
+          },
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService,AwsStrategy,JwtStrategy],
+  providers: [AuthService, AwsStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
