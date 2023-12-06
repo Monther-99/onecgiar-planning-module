1add9fcdd6dc75f23c13f2cf917eff52ec07a043
moayadnajd <moayad.najd@gmail.com>
2023-12-06 11:56:48 +0300

    Env

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     socket_path:"/socket.io",
     socket_url:"https://api.planning.cgiar.org",
     api_url:'https://api.planning.cgiar.org',
-    aws_cognito_link:'https://ost-toc.auth.us-east-1.amazoncognito.com',
-    aws_cognito_client_id:'7j40ekbpnbq4aegcsgjmm66c90',
+    aws_cognito_link:'https://osttoc.auth.us-east-1.amazoncognito.com',
+    aws_cognito_client_id:'1qpvt3bdablluaf8aphovpglna',
     aws_cognito_client_redirect_uri:'https://planning.cgiar.org/auth'
 };
